b138192d8ee69a0e23ff212ae3b4e193a85884d3
AhmadjanovD <halilullahahmadjanov@gmail.com>
2022-08-13 06:50:41 +0900

    jcsdd

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const mongoDB = require('./db')
+const mongoDB = require('./db');
+const user = require('./models/user');
 
+//Database connection
 mongoDB()
 
 const app = express();
 //Body parser
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-
-
+//Port
 app.listen(3002, () => console.log('Server listening on Port: 3002'))
 
-app.get('/api', (req, res) => {
-    res.json({
-        message: 'success',
-        data: 'Hello from Backend'
-    })
-})
+
+app.use('/api', require('./routes/signUp'))
+// app.get('/api', (req, res) => {
+//     res.json({
+//         message: 'success',
+//         data: 'Hello from Backend'
+//     })
+// })
